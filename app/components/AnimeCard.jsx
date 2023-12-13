566aaf208d89479b9c00c8d0431942c7f4e8007a
adriendelagneau <delagneauadrien@yahoo.fr>
2023-12-13 14:37:04 +0100

    first commit

--- app/components/AnimeCard.jsx
+++ app/components/AnimeCard.jsx
@@ -1,9 +1,26 @@
 import Image from "next/image";
+import { MotionDiv } from "./MotionDiv";
+
+const variants = {
+  hidden: { opacity: 0 },
+  visible: {opacity: 1}
+  
+}
 
 
-function AnimeCard({ anime }) {
+function AnimeCard({ anime, index }) {
   return (
-    <div className="max-w-sm rounded relative w-full border-2">
+    <MotionDiv
+      variants={variants}
+      initial="hidden"
+      animate="visible"
+      transition={{
+        delay: (index % 8) * 0.1,
+        ease: "easeInOut",
+        duration: 0.5
+      }}
+      className="max-w-sm rounded relative w-full border-2"
+    >
       <div className="relative w-full h-[37vh]">
         <Image
           src={anime.imgUrl}
@@ -13,7 +30,7 @@ function AnimeCard({ anime }) {
         />
       </div>
     
-      </div>
+      </MotionDiv>
   );
 }
 
